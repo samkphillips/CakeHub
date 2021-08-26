1a156c4fa456334b6978084745cf6779cad9f063
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-08-26 15:52:25 -0400

    commenceRecipes no api

--- client/src/pages/Recipes.js
+++ client/src/pages/Recipes.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import RecipeCard from '../components/RecipeCard'
+import axios from 'axios'
 
 const Recipes = () => {
+  // const { recipes, setRecipes } = useState([])
+
+  // const getRecipes = async () => {
+  //   const res = await axios.get('http://localhost:3001/api')
+  //   setRecipes(res)
+  //   console.log
+  // }
+
+  // useEffect(() => {
+  //   getRecipes()
+  // }, [])
+
   return (
-    <div>
-      <h1>Recipes</h1>
-    </div>
+    // <div>
+    //   <div className="recipes">
+    //     <h2>Recipes</h2>
+    //     <section className="recipe-container">
+    //       {recipes.map((recipe) => (
+    //         <RecipeCard
+    //           title={recipe.title}
+    //           description={recipe.description}
+    //           image={recipe.image}
+    //           duration={recipe.cooking_duration}
+    //           ingredients={recipe.ingredients}
+    //           steps={recipe.steps}
+    //           author={recipe.authorName}
+    //         />
+    //       ))}
+    //     </section>
+    //   </div>
+    // </div>
+    <h2>Hello</h2>
   )
 }
 
