530a7a4634baf78eb5307e3aab4633336fbb8adb
Janay Anguiano <janayanguiano@gmail.com>
2021-08-26 12:36:00 -0700

    commenceSubmit textfields

--- client/src/pages/SubmitRecipes.js
+++ client/src/pages/SubmitRecipes.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
-const SubmitRecipes = () => {
+function SubmitRecipes() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(`Thank you for your submission`)
+  }
   return (
     <div>
-      <h1>SubmitRecipes</h1>
+      <h1>Submit Your Recipe</h1>
+      <h4>Click the submit button below.</h4>
+      <form id="usrform" className="form-container" onSubmit={handleSubmit}>
+        <p>Please enter the name of your recipe.</p>
+        <input name="title" type="" />
+        <p>Include recipe URL.</p>
+        <input name="image" />
+        <p>Include the duration of your recipe (minutes).</p>
+        <input name="duration" />
+        <p>Please include the ingredients of your recipe.</p>
+        <input name="description" />
+        <p>Inform us step by step how to make your recipe.</p>
+        <input name="steps" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
